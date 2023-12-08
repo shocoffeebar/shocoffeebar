558b0153c585d6ead2bbd7239e7aa9be3fc431b4
Logan Olson <loganolsonn@gmail.com>
2023-12-07 22:43:08 -0600

    Changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Quicksand } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const quickSand = Quicksand({
+  subsets: ['latin-ext'],
+});
 
 export const metadata: Metadata = {
   title: 'Sho Coffee & Bar',
@@ -16,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={quickSand.className}>{children}</body>
     </html>
   );
 }
