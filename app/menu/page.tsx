54b29704b3a387b953444608744845769be1ca57
Lowgy <loganolsonn@gmail.com>
2024-03-04 21:52:43 -0600

    Mobile responsiveness for menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -11,18 +11,18 @@ export default function Menu() {
           <img
             src="/beans.png"
             alt="Sho Coffee & Bar"
-            className="z-10 h-[550px] w-[548px] absolute top-0 -left-20"
+            className="z-10 h-[550px] w-full md:w-[548px] absolute top-0 -left-40 md:-left-20"
           />
         </div>
-        <div className="relative flex flex-col p-40 z-20 ">
-          <h2 className="text-[48px] p-2 font-[500]">
+        <div className="relative flex flex-col ml-2 my-48 md:p-40 z-20 ">
+          <h2 className="text-[28px] md:text-[48px] p-2 font-[500]">
             SORRY MENU ISN&apos;T READY YET
           </h2>
-          <p className="text-[28px] p-2 font-[300]">
+          <p className="text-[18px] md:text-[28px] p-2 font-[300]">
             We&apos;re diligently crafting our menu to bring you an exceptional
             dining experience.
           </p>
-          <p className="text-[28px] p-2 font-[300]">
+          <p className="text-[18px] md:text-[28px] p-2 font-[300]">
             Follow us on socials to stay tuned!
           </p>
         </div>
