e86893734b8474c4aba769b0cdbe9ed6618c74e1
Lowgy <loganolsonn@gmail.com>
2024-03-17 18:09:51 -0500

    Policy responsiveness

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -2,29 +2,35 @@ export default function Policy() {
   return (
     <main className="flex min-h-screen flex-col bg-[#252525] text-white lg:px-72 py-12">
       <div className="flex flex-col gap-[30px]">
-        <h1 className="font-[500] text-[48px]">COMPANY POLICY</h1>
-        <h2 className="font-[500] text-[36px]">Uniform</h2>
-        <h3 className="font-[300] text-[28px]">T-Shirt and apron</h3>
-        <p className="font-[300] text-[20px]">
+        <h1 className="font-[500] text-[28px] md:text-[48px]">
+          COMPANY POLICY
+        </h1>
+        <h2 className="font-[500] text-[20px] md:text-[36px]">Uniform</h2>
+        <h3 className="font-[300] text-[18px] md:text-[28px]">
+          T-Shirt and apron
+        </h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           You are required to wear the hat, shirt, and apron issued to you. Your
           hat and shirt should be kept clean and shouldn’t be altered in style
           in any way. You should always be wearing a Sho Coffee & Bar apron
           while on the floor.
         </p>
-        <h3 className="font-[300] text-[28px]">Shoes</h3>
-        <p className="font-[300] text-[20px]">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">Shoes</h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           It is required by the time your second pay-period has ended that you
           will be wearing non-slip shoes while on the floor. They will need to
           be cleaned regularly and thoroughly.
         </p>
-        <h3 className="font-[300] text-[28px]">Bottoms</h3>
-        <p className="font-[300] text-[20px]">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">Bottoms</h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           You are expected to wear black bottoms while on the floor. Jeans are
           also acceptable, but please keep our aesthetic in mind. Your clothes
           must be clean at the beginning of your shift.
         </p>
-        <h3 className="font-[300] text-[28px]">Not permitted:</h3>
-        <ul className="font-[300] text-[20px] list-disc px-12">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">
+          Not permitted:
+        </h3>
+        <ul className="font-[300] text-[16px] md:text-[20px] list-disc px-12">
           <li>
             pants or shorts that are excessively ripped/distressed or are ripped
             in areas that are inappropriate
@@ -36,20 +42,24 @@ export default function Policy() {
             arms are to your sides
           </li>
         </ul>
-        <h3 className="font-[300] text-[28px]">Body Modifications</h3>
-        <p className="font-[300] text-[20px]">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">
+          Body Modifications
+        </h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           You do not need to inform your shift leads that you are getting a new
           mod. If you are concerned whether a mod is permitted to be uncovered
           do not hesitate to ask!
         </p>
-        <h3 className="font-[300] text-[28px]">Hair</h3>
-        <p className="font-[300] text-[20px]">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">Hair</h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Hair that meets or goes past the collar is required to be tied
           securely in a bun or a braid. Facial hair must be kept trimmed or be
           kept in a hair net.
         </p>
-        <h3 className="font-[300] text-[28px]">Personal Hygiene</h3>
-        <p className="font-[300] text-[20px]">
+        <h3 className="font-[300] text-[18px] md:text-[28px]">
+          Personal Hygiene
+        </h3>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Hygiene is important, not only for the comfort of your coworkers but
           for the health and safety of our environment. We are all working in
           close quarters and in a warm kitchen, please plan accordingly. <br />{' '}
@@ -57,14 +67,14 @@ export default function Policy() {
           If you are choosing to wear fragrance, you should wear as little as
           you can.
         </p>
-        <p className="font-[300] text-[20px]">
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Violations of the uniform policy may lead to write ups, shift
           reductions, or termination.
         </p>
-        <h2 className="font-[500] text-[36px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
           Representing SHO Coffee & Bar
         </h2>
-        <p className="font-[300] text-[20px]">
+        <p className="font-[300] text-[16px] md:text-[20px]">
           When you are in your uniform, you are representing our company- inside
           the store, when you are outside, or when you are on social media. In
           uniform, you are expected to act and speak with the same level of
@@ -72,12 +82,14 @@ export default function Policy() {
           other stores, you must be in the office. Smoking and vaping must be
           done out the back door and without your uniform logo visible.
         </p>
-        <p className="font-[300] text-[20px]">
+        <p className="font-[300] text-[16px] md:text-[20px]">
           On the floor, all conversation must be appropriate. If someone is
           making you uncomfortable, inform your shift lead immediately.
         </p>
-        <h2 className="font-[500] text-[36px]">Shift Punctuality</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Shift Punctuality
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Sho Coffee & Bar requires all employees to arrive five to ten minutes
           before the start of their shifts. If you are unable to arrive at the
           expected time, you are responsible to inform the shift lead (by phone
@@ -88,8 +100,10 @@ export default function Policy() {
           fully ready to begin your work. You may not clock in early unless
           requested by your shift lead.
         </p>
-        <h2 className="font-[500] text-[36px]">On-Call Shifts</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          On-Call Shifts
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           On-call shifts are required by Sho Coffee & Bar. You are expected to
           come within an hour of being called, or at the start time of the shift
           you are covering. You do not have to come in if it is past the end of
@@ -99,8 +113,10 @@ export default function Policy() {
           Any time you are called in, you receive a free meal (up to $16) in
           thanks for helping the floor!
         </p>
-        <h2 className="font-[500] text-[36px]">Calling in Sick</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Calling in Sick
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           When you are not feeling able to work, especially when you are ill
           with something contagious, we fully encourage you to stay home or get
           the medical attention you need. We will welcome you back whenever you
@@ -112,13 +128,15 @@ export default function Policy() {
           may result in loss of shifts or termination. Any personal emergency
           will follow the same procedures.
         </p>
-        <h2 className="font-[500] text-[36px]">Switching Shifts</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Switching Shifts
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           You are allowed to swap shifts with other staff members at any time.
           There are a few requirements to facilitate the best outcome for
           adjusting the schedule:
         </p>
-        <ul className="font-[300] text-[20px] list-disc px-12">
+        <ul className="font-[300] text-[16px] md:text-[20px] list-disc px-12">
           <li>Swaps must be agreed to by both parties</li>
           <li>
             Swaps can only be done with staff thatt are the same skill level
@@ -133,8 +151,10 @@ export default function Policy() {
             your shift leads can help you find who may be available
           </li>
         </ul>
-        <h2 className="font-[500] text-[36px]">Booking Time Off</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Booking Time Off
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           While we always try our best to fulfill all requests, there isn’t a
           guarantee that you will receive the time off. Time off requests must
           be made at least two weeks in advance. We prefer you also include a
@@ -148,13 +168,15 @@ export default function Policy() {
           requests approved. Employee needs will be considered when making the
           schedule.
         </p>
-        <h2 className="font-[500] text-[36px]">Extended time off</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Extended time off
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Sho Coffee & Bar can be accommodating for those who need extended time
           away. Whatever the reason may be (travelling, personal time, etc),
           please be aware of the following:
         </p>
-        <ul className="font-[300] text-[20px] list-disc px-12">
+        <ul className="font-[300] text-[16px] md:text-[20px] list-disc px-12">
           <li>
             Extended time off (away for more than three weeks) must be requested
             three months in advance
@@ -165,12 +187,14 @@ export default function Policy() {
             your absence!
           </li>
         </ul>
-        <p className="font-[300] text-[20px]">
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Failure to inform us in a timely manner may lead to loss of shifts or
           termination.
         </p>
-        <h2 className="font-[500] text-[36px]">Employee Breaks</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Employee Breaks
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           For shifts over five hours, you are entitled to a thirty minute unpaid
           break. At no time will we allow you to take breaks for smoking/
           vaping, texting, snacking outside of your one unpaid break. <br />{' '}
@@ -182,8 +206,8 @@ export default function Policy() {
           syrups, powders, etc. All beverages must be kept out of customer
           sight.
         </p>
-        <h2 className="font-[500] text-[36px]">Discounts</h2>
-        <ul className="font-[300] text-[20px] list-disc px-12">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">Discounts</h2>
+        <ul className="font-[300] text-[16px] md:text-[20px] list-disc px-12">
           <li>All discounts must be authorized by a member of management</li>
           <li>Regular staff will recieve one discount of 50% (or up to $16)</li>
           <li>
@@ -197,18 +221,20 @@ export default function Policy() {
             theft.
           </li>
         </ul>
-        <h2 className="font-[500] text-[36px]">Cell phones</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">Cell phones</h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Cell phone use is prohibited when you are clocked in. If you are
           expecting an important call, let your shift lead know when.
         </p>
-        <h2 className="font-[500] text-[36px]">Clocked Out Employees</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">
+          Clocked Out Employees
+        </h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           When you are not clocked in, you are not permitted on the
           floor/kitchen. Keep to the office or stay in the lobby.
         </p>
-        <h2 className="font-[500] text-[36px]">Tips</h2>
-        <p className="font-[300] text-[20px]">
+        <h2 className="font-[500] text-[20px] md:text-[36px]">Tips</h2>
+        <p className="font-[300] text-[16px] md:text-[20px]">
           Tips will be distributed amongst staff. All tips will be added to your
           paycheck.
         </p>
