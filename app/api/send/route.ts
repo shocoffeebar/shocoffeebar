991d76c01b3861b38bc55e425a25cdf908389040
Logan Olson <loganolsonn@gmail.com>
2024-03-23 19:46:46 -0500

    Setting up new environment

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { ContactEmailTemplate } from '@/components/contact-template';
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -15,35 +15,35 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'ShoCoffeeBar <info@shocoffeebar.ca>',
-      to: [`${email}`],
-      subject: `Thank you for contacting Sho Coffee Bar`,
-      react: ContactEmailTemplate({
-        name,
-        email,
-        phone,
-        subject,
-        message,
-        type,
-      }),
-    });
+  // try {
+  //   const { data, error } = await resend.emails.send({
+  //     from: 'ShoCoffeeBar <info@shocoffeebar.ca>',
+  //     to: [`${email}`],
+  //     subject: `Thank you for contacting Sho Coffee Bar`,
+  //     react: ContactEmailTemplate({
+  //       name,
+  //       email,
+  //       phone,
+  //       subject,
+  //       message,
+  //       type,
+  //     }),
+  //   });
 
-    if (error) {
-      return NextResponse.json(
-        { message: 'Email sending failed', error },
-        { status: 400 }
-      );
-    }
-    return NextResponse.json(
-      { message: 'Email sent successfully', data },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: 'Email sending failed', error },
-      { status: 500 }
-    );
-  }
+  //   if (error) {
+  //     return NextResponse.json(
+  //       { message: 'Email sending failed', error },
+  //       { status: 400 }
+  //     );
+  //   }
+  //   return NextResponse.json(
+  //     { message: 'Email sent successfully', data },
+  //     { status: 200 }
+  //   );
+  // } catch (error) {
+  //   return NextResponse.json(
+  //     { message: 'Email sending failed', error },
+  //     { status: 500 }
+  //   );
+  // }
 }
