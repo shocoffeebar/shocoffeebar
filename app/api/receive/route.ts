9c29bc0c90d0bedeab30eda8d74fae06499a4acb
Logan Olson <loganolsonn@gmail.com>
2024-04-03 19:23:52 -0500

    More email setup

--- app/api/receive/route.ts
+++ app/api/receive/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_APP_PASS,
       },
     });
 
