0912b59b987461354d328f9c2da732d505ee0d79
Stellce <andrey.is323@gmail.com>
2024-07-30 18:44:37 +0200

    Redirect on /menu

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/menu',
+            destination: 'https://utfs.io/f/83b6086e-f498-4a0f-9024-8413de091645-a0646z.pdf',
+            permanent: false,
+            basePath: false
+          },
+        ]
+      },
+}
 
 module.exports = nextConfig
