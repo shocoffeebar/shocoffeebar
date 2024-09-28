555faca1455bf060b0298f2f2034f4a0092a72ec
Serhii O. Pashchenko <105823386+s-o-pashchenko@users.noreply.github.com>
2024-09-28 18:39:50 -0500

    new menu (#16)

--- config/external-links.mjs
+++ config/external-links.mjs
@@ -1,4 +1,4 @@
 export const externalLinks = {
-  menuLink: 'https://utfs.io/f/gOOgxymKkZ4q2rg1mKBHsqTzw8IC60eBYSWDu5agpPf7tF4R',
+  menuLink: 'https://utfs.io/f/gOOgxymKkZ4q2Y7ylYBHsqTzw8IC60eBYSWDu5agpPf7tF4R',
   // ...other properties...
 };
